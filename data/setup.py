603016041443e5dd09caf899985ac250bfbaa9e6
michaelsekatchev@live.ca <michaelsekatchev@live.ca>
2024-01-07 08:02:53 +0000

    adding files

---
+++ data/setup.py
@@ -0,0 +1,17 @@
+'''
+.
+'''
+
+from setuptools import setup, find_packages
+
+setup(
+    name='utilities',
+    version='0.1',
+    description="Andrew Wetzel's utitilies package",
+    url="https://bitbucket.org/awetzel/utilities",
+    author='Andrew Wetzel, packaged for personal use by SheaGK',
+    author_email='sheagk@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False
+)
