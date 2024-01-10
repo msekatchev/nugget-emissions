1a9b11442800a1e081dec30ad2b965c2a92f5354
michaelsekatchev@live.ca <michaelsekatchev@live.ca>
2024-01-10 07:19:25 +0000

    improving nearest voxel id search

--- processing/.ipynb_checkpoints/nearest-voxel-id-search-checkpoint.py
+++ processing/.ipynb_checkpoints/nearest-voxel-id-search-checkpoint.py
@@ -2,22 +2,31 @@ import numpy as np
 from scipy.spatial import distance
 from concurrent.futures import ThreadPoolExecutor
 import concurrent.futures
+import sys
+
+if len(sys.argv) != 2:
+    print("Need an extra string to specify which voxel coords file to use")
+    sys.exit(1)
+
+# example: ioni_gas
+name = sys.argv[1]
+
 
 location_grid_coords = "../data/FIRE/grid-coords.npy"
-location_non_empty_voxel_coords = "../data/FIRE/non-empty-coords-ioni_gas.npy"
-name = location_non_empty_voxel_coords[30:][0:location_non_empty_voxel_coords[30:].rfind(".")]
+location_non_empty_voxel_coords = "../data/FIRE/non-empty-coords-"+name+".npy"
+# name = location_non_empty_voxel_coords[30:][0:location_non_empty_voxel_coords[30:].rfind(".")]
 location_save_grid_ids = "../data/FIRE/grid-ids--"+name+".npy"
 
-grid_c = np.load('grid_c.npy')
+grid_c = np.load(location_grid_coords)
 print(f">> loaded in voxel grid coordinates from {location_grid_coords}")
 
-non_empty_points = np.load('non_empty_points.npy')
+non_empty_points = np.load(location_non_empty_voxel_coords)
 print(f">> loaded in non-empty voxel coordinates from {location_non_empty_voxel_coords}")
 
-
+print(f">> {name}")
 
 # Parameters
-batch_size = 10000
+batch_size = 100
 
 # Function to calculate distances and update grid_ids for a given interval
 def process_interval(start, end):
@@ -37,7 +46,7 @@ with ThreadPoolExecutor() as executor:
 
     for future in concurrent.futures.as_completed(futures):
         interval_min, interval_max = futures[future]
-        print(interval_min,interval_max,end=", ")
+        print(f"Working on batch [{interval_min}-{interval_max}] out of {len(grid_c)}, so {interval_max/len(grid_c)*100}% done.", end="\r")
         # print(f"Processed interval [{interval_min}:{interval_max}]")
 
 
