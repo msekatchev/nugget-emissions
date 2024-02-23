830ab94c3ac5c2af42cbbe9d59b76ec8ed4dc891
michaelsekatchev@live.ca <michaelsekatchev@live.ca>
2024-02-23 07:41:21 +0000

    added backup script

---
+++ backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Add all files except those in the data/FIRE directory
+git add --all -- ':!data/FIRE'
+
+# Commit changes with the provided message
+git commit -m "$1"
+
+# Push changes to remote repository
+git push
