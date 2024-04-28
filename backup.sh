bdc9aacf0fc2be97f679fd653b835deb01785911
michaelsekatchev@live.ca <michaelsekatchev@live.ca>
2024-04-28 20:23:12 +0000

    generating skymaps

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 # Add all files except those in the data/FIRE directory
-git add --all -- ':!data/FIRE' ':!data/skymap_integration_cube_indexes.npy'
+git add --all -- ':!data/FIRE'
 
 # Commit changes with the provided message
 git commit -m "$1"
